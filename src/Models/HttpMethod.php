61d094931fef311099b0cf112134b75c93028f99
ChristianGroeber <christian.groeber@bluewin.ch>
2022-10-15 18:44:00 +0200

    consts

---
+++ src/Models/HttpMethod.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nacho\Models;
+
+class HttpMethod
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const HEAD = 'HEAD';
+    const PUT = 'PUT';
+    const DELETE = 'DELETE';
+}
